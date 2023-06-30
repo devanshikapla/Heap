04ab5b9ac008d3e2d646c7dc330c3d08d1b29c37
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-06-30 21:02:50 +0530

    Create Nearly sorted array.cpp

---
+++ Nearly sorted array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        vector<int> ans;
+        priority_queue <int, vector<int>, greater<int> > pq;
+        for(int i=0; i < num ; i++){
+           pq.push(arr[i]);
+           
+        }
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
